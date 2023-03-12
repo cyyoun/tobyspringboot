a922a1867d48a466fdd1a63de097bc928480644c
cyyoun <cyyoun41@gmail.com>
2023-03-12 15:24:38 +0900

    Front Controller 전환

--- src/main/java/tobyspring/newhelloboot/HelloController.java
+++ src/main/java/tobyspring/newhelloboot/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/hello") // URL이 /hello로 들어오는 것과 동시에 http 메서드명이 'GET'이어야 함
     public String hello(String name){
         return "Hello "+name;
 
--- src/main/java/tobyspring/newhelloboot/NewhellobootApplication.java
+++ src/main/java/tobyspring/newhelloboot/NewhellobootApplication.java
@@ -3,6 +3,10 @@ package tobyspring.newhelloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,14 +19,26 @@ public class NewhellobootApplication {
 	public static void main(String[] args) {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hallo", new HttpServlet() {
+			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					resp.setStatus(200);
-					resp.setHeader("Context-Type", "text/plain");
-					resp.getWriter().println("Hello cyyoun");
+					// 인증, 보안, 다국어, 공통 기능
+					if(req.getRequestURI().equals("/hello") && req.getMethod().equals((HttpMethod.GET.name()))) {
+						String name = req.getParameter("name"); // name 이라는 파라미터 값을 반환해서 저장함
+
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println("Hello "+name);
+					}
+					else if(req.getRequestURI().equals("/user")){
+						//
+					}
+					else{
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
+
 				}
-			}).addMapping("/hello");
+			}).addMapping("/*"); //모든 요청을 다 받게 변경
 		});
 		webServer.start();
 
